82fe6f6e9c8e966f0f0159acbc155430d7e20aaf
Chrs Boyd <chrisboyd360@gmail.com>
2020-10-24 19:54:44 -0400

    finished step 1 and 2
    polynomial degree 6 has best combination of fit and simplicity

--- Linear_Regression/ERM.m
+++ Linear_Regression/ERM.m
@@ -1,13 +1,31 @@
 x = load('dataset1_inputs.txt');
 t = load('dataset1_outputs.txt');
+w = 30;
+errors = zeros(1,w) ;
+for i=1:w    
+    %polynomial curve fitting
+    p = polyfit(x,t,i);
+    results = polyval(p,x);
+    errors(i) = sum((results - t).^2);
+end
+figure;
+plot([1:1:w],errors,'-*');
+title('ERM, sum square loss vs polynomial degree');
+xlabel('Degree of polynomial');
+ylabel('Empirical Square Loss');
 
 figure;
 
-
-%polynomial curve fitting
-%for degrees W[1,30], .5 sum(1..30)(y(xn,w)-tn^2)
-p = polyfit(x,t,6);
 x2 = [-1:.1:1];
-result = polyval(p,x2);
+plot(x,t,'+',x2,polyval(polyfit(x,t,6),x2));
 
-plot(x,t,'+',x2,result);
+title('Sample output vs input data');
+xlabel('input data');
+ylabel('output data');
+legend('output','W=6');
+
+
+
+
+
+
