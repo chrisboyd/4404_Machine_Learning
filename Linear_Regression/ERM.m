7741d4b7f043a1442e71700a8c27a71f00e786d2
Chrs Boyd <chrisboyd360@gmail.com>
2020-10-25 12:08:54 -0400

    complete
    final decision after seeing effect of regularization parameter on polynomial degree is to use degree 5 for the model

--- Linear_Regression/ERM.m
+++ Linear_Regression/ERM.m
@@ -5,6 +5,9 @@ errors = zeros(1,w);
 for i=1:w    
     %polynomial curve fitting
     p = polyfit(x,t,i);
+    if w == 6
+        psix = p;
+    end
     results = polyval(p,x);
     errors(i) = sum(0.5 .* (results - t).^2);
 end
@@ -14,18 +17,30 @@ title('ERM, sum square loss vs polynomial degree');
 xlabel('Degree of polynomial');
 ylabel('Empirical Square Loss');
 
+%step 5 visualization
+%plot of learned polynomials
 figure;
-
 x2 = [-1:.1:1];
-plot(x,t,'+',x2,polyval(polyfit(x,t,6),x2));
-
+plot(x,t,'+');
+hold on;
+plot(x2,polyval(polyfit(x,t,1),x2));
+plot(x2,polyval(polyfit(x,t,5),x2));
+plot(x2,polyval(polyfit(x,t,10),x2));
+plot(x2,polyval(polyfit(x,t,20),x2));
+plot(x2,polyval(polyfit(x,t,30),x2));
+ylim([-2 2]);
 title('Sample output vs input data');
-xlabel('input data');
-ylabel('output data');
-legend('output','W=6');
+xlabel('input value');
+ylabel('output value');
+legend('output','W=1','W=5','W=10','W=20','W=30');
+hold off;
 
-
-
-
-
-
+%plot of errors for W=1,5,10,20,30
+data = [errors(1),errors(5),errors(10),errors(20),errors(30)];
+figure;
+hold on;
+plot([1,5,10,20,30],data,'-*');
+title('ERM, sum square loss vs polynomial degree');
+xlabel('Degree of polynomial');
+ylabel('Empirical Square Loss');
+hold off
--- Linear_Regression/cross_validation.m
+++ Linear_Regression/cross_validation.m
@@ -2,23 +2,40 @@ x = load('dataset1_inputs.txt');
 t = load('dataset1_outputs.txt');
 
 m = [x t];
-
+errors = zeros(1,15);
 table = array2table(m,'VariableNames',{'input','output'});
 % Nonstratified partition
-hpartition = cvpartition(100,'Holdout',0.1); 
-%get a column vector, 1 indicates include row in training
-idxTrain = training(hpartition);
-%get rows identified for training from table
-tblTrain = table(idxTrain,:);
-%use remaining rows of table for test partition
-idxNew = test(hpartition);
-tblNew = table(idxNew,:);
+hpartition = cvpartition(100,'KFold',10);
 
-%for each set of 10 training points, train a model of
-%W=1..10 
-set1 = table2array(tblTrain);
-x2 = [-1:.1:1];
+%loop through each degree of polynomial for model
+for w=3:15
+    %loop through using each of 10 subsets as testing data, while
+    %remaining 9 subsets are all used as training data
+    for i=1:10
+        %get a column vector, 1 indicates include row in training
+        %idxTrain is the ith training set
+        idxTrain = training(hpartition,i);
+        %get rows identified for training from table
+        tblTrain = table(idxTrain,:);
+        %use remaining rows of table for test partition
+        idxNew = test(hpartition,i);
+        tblTest = table(idxNew,:);
 
+        %fit polynomial
+        p = polyfit(tblTrain.input,tblTrain.output,w);
+        %get predicted output for training data
+        pred = polyval(p, tblTest.input);
+        errors(w) =  errors(w) + sum(0.5 .* (pred - tblTest.output).^2);
+    end
+    errors(w) = errors(w) / 10;
+end
+%plot average errors for each of w=[3,8] against w=[3,8]
+
+figure;
+plot([3:1:15],errors(3:15),'-*');
+title('10-Fold Cross-Validation ERM Error vs Polynomial Degree');
+xlabel('Degree of polynomial');
+ylabel('Average Empirical Square Loss');
 
 % plot(set1(1:10,1),set1(1:10,2),'+',x2,polyval(polyfit(set1(1:10,1),set1(1:10,2),8),x2));
 % ylim([-2 2]);
