897f166ad9e9474091effcc07e14754b66d75244
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-02 16:29:50 -0800

    comparison of in and out
    Shows input data set and classification (+/-)

--- Perceptron/perceptron.m
+++ Perceptron/perceptron.m
@@ -1,11 +1,15 @@
 input = load('6pointsinputs.txt');
 output = load('6pointsoutputs.txt');
-in_x = input(:,1);
-in_y = input(:,2);
+input(:,3) = output;
+positive = input(input(:,3)==1,:);
+negative = input(input(:,3)==-1,:);
 figure;
-plot(in_x, in_y,'*');
+plot(positive(:,1),positive(:,2) ,'+');
+hold on;
+plot(negative(:,1),negative(:,2) ,'o');
 ax = gca;
 ax.XAxisLocation = 'origin';
 ax.YAxisLocation = 'origin';
+legend('positive output', 'negative output');
 xlim([-3,3]);
 ylim([-3,3]);
