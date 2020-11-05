1b36a9f39bbc33e8108b2bd5d3e3112534cae1fd
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-04 20:30:00 -0800

    basic perceptron algorithm
    runs 1 update on base input, gets weight vectors.
    
    find norms of each weight vector, how to plot

--- Perceptron/perceptron.m
+++ Perceptron/perceptron.m
@@ -1,7 +1,28 @@
 input = load('6pointsinputs.txt');
 output = load('6pointsoutputs.txt');
+full = input;
+full(:,3) = output;
+positive = full(full(:,3)==1,:);
+negative = full(full(:,3)==-1,:);
 
-weights = test(input,output);
+figure;
+plot(positive(:,1),positive(:,2) ,'+');
+hold on;
+plot(negative(:,1),negative(:,2) ,'o');
+ax = gca;
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+legend('Positive Classification', 'Negative Classification');
+title('Plot of Input Vectors and Expected Classification');
+xlim([-3,3]);
+ylim([-3,3]);
+
+weights = zeros(6,3);
+iterations = zeros(6,1);
+
+for i=1:6
+    [weights(i,:), iterations(i)] = test(input,output);
+end
 
 
 function [weights, iterations] = test(input, labels)
@@ -15,16 +36,20 @@ des_matr(:,3) = 1;
 weights = zeros(1, cols+1);
 
 
-iterations = 1;
+iterations = 0;
 error = -1;
 
-while error ~= 0 || iterations > 6
-    sign = dot(weights, shuffle_in(1,1:2));
+while error ~= 0 && iterations <= 6
     iterations = iterations + 1;
-    if (sign * 
+    sign = dot(weights, des_matr(iterations,:));
+    
+    if sign == 0;
+        weights = weights + labels(iterations) * des_matr(iterations,:);
+    else (sign * labels(iterations) > 0);
+        error = 0;
+    end
     
 end
 
 
-pause;
 end
