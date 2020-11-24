f35efd0de4ea7d6daddf0478eb8b5a039f9a0bc5
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-24 15:38:38 -0800

    Update binary_loss.m
    fixed error where binary loss was being recorded as negative

--- Perceptron/binary_loss.m
+++ Perceptron/binary_loss.m
@@ -1,12 +1,16 @@
 function bloss = binary_loss(weight, data, labels)
 %compute average binary loss for given weight vector over all 
 %inputs and respective labels
-bloss = 0;
-for i=1:size(labels)
-    sign = dot(weight,data(i,:)) * labels(i);
-    sign = sign / abs(sign);
-    bloss = bloss + (.5 * sign - .5);
-end
+	bloss = 0;
+	for i=1:size(labels)
+	    sign = dot(weight,data(i,:)) * labels(i);
+	    sign = sign / abs(sign);
+	    sign = (.5 * sign - .5);
+	    if sign ~= 0
+	        sign = 1;
+	    end
+	    bloss = bloss + sign;
+	end
 
-bloss = bloss / i;
-end
+	bloss = bloss / i;
+	end
