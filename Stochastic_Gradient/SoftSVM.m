c44fac6a446c750b25e05c39c26c8a7e864799d3
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-25 16:35:41 -0800

    complete
    fixed a bug where updated on weight vector was incorrect due to incorrect brackets on hyperparameter

--- Stochastic_Gradient/SoftSVM.m
+++ Stochastic_Gradient/SoftSVM.m
@@ -12,26 +12,24 @@ i = 1;
 b_loss = zeros(1,T);
 h_loss = zeros(1,T);
 for j = 1:T
-    w = (1/lambda * j) * theta;
-    i = ceil(rand * numel(labels));
+    w = (1/(lambda * j)) * theta;
+    i = floor(rand * numel(labels)) + 1;
     update = labels(i) * dot(w, data(i,:));
     if update < 1
         theta = theta + labels(i) * data(i,:);
-    else
-        theta = theta;
     end
     b_loss(j) = binary_loss(w,data,labels);
     h_loss(j) = logistic_loss(w,data,labels);
 end
 
 figure;
-x = 1:1:500;
+x = 1:1:T;
 plot(x,b_loss);
-title('Binary Loss of SGD for SoftSVM for lambda = .001');
+title('Binary Loss of SGD for SoftSVM for lambda = 0.001');
 xlabel('iteration');
 ylabel('loss');
 figure;
 plot(x,h_loss);
-title('Hinge Loss of SGD for SoftSVM for lambda = .001');
+title('Hinge Loss of SGD for SoftSVM for lambda = 0.001');
 xlabel('iteration');
 ylabel('loss');
